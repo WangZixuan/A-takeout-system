1c4f8fcf4e3654fb5e53b0362b3d0b90bf092bec
SCP233 <997071897@qq.com>
2015-06-24 15:19:36 +0800

    add addDishes.php

--- php/addDishes.php
+++ php/addDishes.php
@@ -7,13 +7,49 @@ if (!$con)
 
 mysql_select_db("takeout", $con);
 
+
+if ((($_FILES["file"]["type"] == "image/gif")
+|| ($_FILES["file"]["type"] == "image/jpeg")
+|| ($_FILES["file"]["type"] == "image/pjpeg"))
+&& ($_FILES["file"]["size"] < 20000))
+  {
+  if ($_FILES["file"]["error"] > 0)
+    {
+    echo "Return Code: " . $_FILES["file"]["error"] . "<br />";
+    }
+  else
+    {
+    echo "Upload: " . $_FILES["file"]["name"] . "<br />";
+    echo "Type: " . $_FILES["file"]["type"] . "<br />";
+    echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />";
+    echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br />";
+	$imgName = $_FILES["file"]["name"];
+
+    if (file_exists("upload/image/" . $_FILES["file"]["name"]))
+      {
+      echo $_FILES["file"]["name"] . " already exists. ";
+      }
+    else
+      {
+      move_uploaded_file($_FILES["file"]["tmp_name"],
+      "upload/" . $_FILES["file"]["name"]);
+      echo "Stored in: " . "upload/" . $_FILES["file"]["name"];
+      }
+    }
+  }
+else
+  {
+  echo "Invalid file";
+  }
+
+
 $getmax = "select id from dishes order by id desc limit 1";
 $result = mysql_query($getmax, $con);
 $nextline = 1+mysql_result($result, 0);
 
 $sql="INSERT INTO dishes (id, name, price, class, explains, image)
 VALUES
-($nextline, '$_POST[name]', '$_POST[price]', '$_POST[classes]', '$_POST[explain]', 'null')";
+($nextline, '$_POST[name]', '$_POST[price]', '$_POST[classes]', '$_POST[explain]', '$imgName')";
 
 if (!mysql_query($sql,$con))
   {
